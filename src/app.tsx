d89c9e679be45fc24a4c13ca3905b6cfb4e44bde
kimingw <m84013885@163.com>
2024-11-21 10:42:49 +0800

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import { useState, useMemo } from 'preact/hooks'
+import { getPinyinForString } from '../core'
+
+export function App() {
+  const [value, setValue] = useState('')
+
+  const pinyin = useMemo(() => {
+    const tokens = getPinyinForString(value)
+    return tokens.map(token => token.target).join(' ')
+  }, [value])
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <input style={{ width: '50%', height: '2rem', fontSize: '1.5rem' }} type="text" value={value} onInput={(e) => setValue((e.target as HTMLInputElement).value)} />
+      <div style={{ fontSize: '1.5rem', paddingTop: '10px' }}>{pinyin}</div>
+    </div>
+  )
+}
